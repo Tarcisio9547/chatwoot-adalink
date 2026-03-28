7d0641f6e7f9177f2c33e0e325dd19fe9b07f298
Syncronia Dev <tarcisio9547@gmail.com>
2026-03-28 10:04:27 -0300

    security: allow cross-site cookies for iframe embed using SameSite=None

--- app/javascript/dashboard/store/utils/api.js
+++ app/javascript/dashboard/store/utils/api.js
@@ -13,7 +13,7 @@ import {
   CHATWOOT_SET_USER,
 } from '../../constants/appEvents';
 
-Cookies.defaults = { sameSite: 'Lax' };
+Cookies.defaults = { sameSite: 'None', secure: true };
 
 export const getLoadingStatus = state => state.fetchAPIloadingStatus;
 export const setLoadingStatus = (state, status) => {
