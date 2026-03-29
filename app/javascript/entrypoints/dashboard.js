843690bf6bb6ca9b423db458a63ce4661706188c
Syncronia Dev <tarcisio9547@gmail.com>
2026-03-28 22:23:21 -0300

    fix: set i18n default locale to pt_BR in BOTH entrypoints (dashboard + v3app)

--- app/javascript/entrypoints/dashboard.js
+++ app/javascript/entrypoints/dashboard.js
@@ -35,7 +35,8 @@ import 'floating-vue/dist/style.css';
 
 const i18n = createI18n({
   legacy: false, // https://github.com/intlify/vue-i18n/issues/1902
-  locale: 'en',
+  locale: 'pt_BR',
+  fallbackLocale: 'en',
   messages: i18nMessages,
 });
 
