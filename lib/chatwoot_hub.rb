00a1279ef64ed898b521d5930025aea3e7b1690a
Syncronia Dev <tarcisio9547@gmail.com>
2026-04-05 11:32:30 -0300

    fix: unlock enterprise features for self-hosted

--- lib/chatwoot_hub.rb
+++ lib/chatwoot_hub.rb
@@ -37,15 +37,15 @@ class ChatwootHub
   end
 
   def self.pricing_plan
-    return 'community' unless ChatwootApp.enterprise?
+    return 'Enterprise' unless ChatwootApp.enterprise?
 
-    InstallationConfig.find_by(name: 'INSTALLATION_PRICING_PLAN')&.value || 'community'
+    InstallationConfig.find_by(name: 'INSTALLATION_PRICING_PLAN')&.value || 'Enterprise'
   end
 
   def self.pricing_plan_quantity
-    return 0 unless ChatwootApp.enterprise?
+    return 999 unless ChatwootApp.enterprise?
 
-    InstallationConfig.find_by(name: 'INSTALLATION_PRICING_PLAN_QUANTITY')&.value || 0
+    InstallationConfig.find_by(name: 'INSTALLATION_PRICING_PLAN_QUANTITY')&.value || 999
   end
 
   def self.support_config
