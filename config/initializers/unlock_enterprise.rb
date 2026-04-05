e0236bde9b02b172b4c894f470c8e7181080667f
Syncronia Dev <tarcisio9547@gmail.com>
2026-04-05 14:26:34 -0300

    fix: safer enterprise unlock initializer

--- config/initializers/unlock_enterprise.rb
+++ config/initializers/unlock_enterprise.rb
@@ -1,17 +1,25 @@
 Rails.application.config.after_initialize do
-  next unless defined?(InstallationConfig)
-
-  InstallationConfig.find_or_create_by(name: 'INSTALLATION_PRICING_PLAN').update(value: 'Enterprise')
-  InstallationConfig.find_or_create_by(name: 'INSTALLATION_PRICING_PLAN_QUANTITY').update(value: '999')
-
-  [2, 3].each do |account_id|
-    account = Account.find_by(id: account_id)
-    next unless account
-
-    account.custom_attributes ||= {}
-    account.custom_attributes['plan_name'] = 'Enterprise'
-    account.save!
+  Rails.application.config.to_prepare do
+    # no-op
   end
-rescue StandardError => e
-  Rails.logger.warn "[unlock_enterprise] #{e.message}"
+
+  ActiveSupport.on_load(:active_record) do
+    begin
+      next unless ActiveRecord::Base.connection.table_exists?('installation_configs')
+
+      InstallationConfig.find_or_create_by(name: 'INSTALLATION_PRICING_PLAN').update_column(:value, 'Enterprise')
+      InstallationConfig.find_or_create_by(name: 'INSTALLATION_PRICING_PLAN_QUANTITY').update_column(:value, '999')
+
+      next unless ActiveRecord::Base.connection.table_exists?('accounts')
+
+      [2, 3].each do |account_id|
+        account = Account.find_by(id: account_id)
+        next unless account
+        attrs = account.custom_attributes || {}
+        account.update_column(:custom_attributes, attrs.merge('plan_name' => 'Enterprise'))
+      end
+    rescue StandardError => e
+      Rails.logger.warn "[unlock_enterprise] #{e.message}"
+    end
+  end
 end
